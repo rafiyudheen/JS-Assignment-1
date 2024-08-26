39d9c7b5af48a901894c23c4c3d0fffeb99822d8
rafiyudheen <mrafiyudheen@gmail.com>
2024-08-26 14:52:19 +0530

    completed

---
+++ script.js
@@ -0,0 +1,12 @@
+//let p=document.getElementById("number")
+let i=0
+while (i<=10)
+{
+    if(i%2==0)
+    {
+        document.write(i )
+        document.write ("<br>"); 
+        console.log(i)
+    }
+    i++
+}
